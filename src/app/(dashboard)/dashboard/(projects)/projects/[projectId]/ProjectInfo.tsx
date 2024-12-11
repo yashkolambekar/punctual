933278747b36bf5ffbaa64eba6e7131ae12c0223
Yash <yashskolambekar2015@gmail.com>
2024-12-11 18:26:23 +0530

    some change in project tiles

--- src/app/(dashboard)/dashboard/(projects)/projects/[projectId]/ProjectInfo.tsx
+++ src/app/(dashboard)/dashboard/(projects)/projects/[projectId]/ProjectInfo.tsx
@@ -2,37 +2,65 @@
 
 import api from "@/lib/api";
 import { IProject } from "@/store/atoms/ProjectsState";
+import { Radio } from "antd";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
+const ProjectInfo = ({ projectId }: { projectId: string }) => {
+  const [project, setProject] = useState<IProject>();
 
-const ProjectInfo = ({projectId} : {projectId: string}) => {
+  const options = [
+    {
+        label: "Year",
+        value: "year"
+    },
+    {
+        label: "Month",
+        value: "month"
+    },
+    {
+        label: "Week",
+        value: "week"
+    },
+    {
+        label: "Day",
+        value: "day"
+    }
+  ]
 
-    const [project, setProject] = useState<IProject>()
+  useEffect(() => {
+    api
+      .get(`/projects?id=${projectId}`)
+      .then((res) => {
+        setProject(res.data.project);
+      })
+      .catch((e) => {
+        console.error(e);
+        toast.error("Failed to get project info");
+      });
+  }, [projectId]);
 
-    useEffect(() => {
-        api.get(`/projects?id=${projectId}`).then((res) => {
-            setProject(res.data.project);
-        }).catch((e) => {
-            console.error(e);
-            toast.error("Failed to get project info");
-        })
-    }, [projectId]);
-
-
-    return (
-        <>
-        <div>
-            <p className="text-[0.7em] font-light m-0">Project Name</p>
-            <p className="text-[2em] font-bold m-0" >{project?.name}</p>
-            {
-                project?.description ? (
-                    <p className="mt-3">{project?.description}</p>
-                ) : ""
-            }
+  return (
+    <>
+      <div className="flex">
+        <div className="w-full">
+          <p className="text-[0.7em] font-light m-0">Project Name</p>
+          <p className="text-[2em] font-bold m-0">{project?.name}</p>
+          {project?.description ? (
+            <p className="mt-1">{project?.description}</p>
+          ) : (
+            ""
+          )}
         </div>
-        </>
-    )
-}
+        <div className="flex-shrink-0">
+            <Radio.Group 
+            options={options}
+            optionType="button"
+            />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ProjectInfo;
